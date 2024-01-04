6b9af13bbc65b627557e51b1821e0a0ec51db1f9
Daniel Meiri <danielt.meiri@gmail.com>
2024-01-04 13:58:08 +0200

    no cors maybe?

--- server.js
+++ server.js
@@ -26,14 +26,14 @@ mongoose.connect(
 );
 
 app.use(morgan('dev'));
-app.use(
-	cors({
-		origin: '*',
-		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-		allowedHeaders: ['Content-Type', 'Authorization'],
-		preflightContinue: false,
-	})
-);
+// app.use(
+// 	cors({
+// 		origin: '*',
+// 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+// 		allowedHeaders: ['Content-Type', 'Authorization'],
+// 		preflightContinue: false,
+// 	})
+// );
 app.use(cookieParser());
 app.use(express.json());
 
